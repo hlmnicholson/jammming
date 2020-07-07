077bd30102435c780e091fd9dce31aee7404e11b
hlmnicholson <nicholson.helena@gmail.com>
2020-07-07 14:47:16 +0200

    file structure and basic component design

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+
+class TrackList extends Component {
+  render() {
+    return (
+      <div class="TrackList">
+        <h1>I'm a track</h1>
+        <h1>I'm a track</h1>
+        <h1>I'm a track</h1>
+      </div>
+    )
+  }
+}
+
+export default TrackList
